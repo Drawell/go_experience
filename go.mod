4f75547c1e5226f2e0113a1a03ea31ba0b31d413
Drawell <ivanevgnikonov@gmail.com>
2026-03-21 17:14:32 +0300

    move calculator to another repo

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go_experience
 
 go 1.25.0
+
+require github.com/Drawell/simple_calc v1.0.1
--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"go_experience/calculator"
+	calcLib "github.com/Drawell/simple_calc/lib"
 )
 
 func main() {
 	text := "2 * (1 + 2 * 3)"
-	value, err := calculator.Evaluate(text)
+	value, err := calcLib.Evaluate(text)
 	fmt.Println(value, err)
 }
